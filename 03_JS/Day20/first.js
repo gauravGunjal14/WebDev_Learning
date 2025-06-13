99ad104324d6938e0b002841149b34192581a028
gaurav-gunjal14 <gauravgunjal45@gmail.com>
2025-06-13 14:15:58 +0530

    Add first js file

---
+++ 03_JS/Day20/first.js
@@ -0,0 +1,23 @@
+// console.log("Hello world")
+
+// Number
+let num = 12;
+console.log(num);
+// String
+let str = "Gaurav"
+console.log(str)
+// constant
+const id = 30;
+console.log(id)
+// boolen
+let statement = true
+console.log(statement)
+// undefined
+let acc;
+console.log(typeof acc)
+//null
+let a=null
+console.log(a)
+// bigint
+let b=384792222222222227813074831874263107430n;
+console.log(b)
