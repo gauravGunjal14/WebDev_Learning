74c089d94de386496e0e54d2a5a81e0e1427caff
gaurav-gunjal14 <gauravgunjal45@gmail.com>
2025-06-15 21:33:02 +0530

    Add day 22

---
+++ 03_JS/Day22/first.js
@@ -0,0 +1,20 @@
+// strings
+
+// let price = 80;
+// console.log(`price of tomato is ${price}`)
+
+// let s1 = "hello"
+// let s2 = "World"
+// let s3 = s1+s2;
+// console.log(s3.length)
+
+// console.log('"hello everyone"');
+
+// console.log("hello \n everyone")
+
+//escape character ----> \
+// console.log("hello \\n everyone")
+
+let s1 = "gaurav"
+console.log(s1[1])
+console.log(s1.charAt(2))
