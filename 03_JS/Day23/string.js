02e10f5f00337194f3f54e50a691f741c55a9841
gaurav-gunjal14 <gauravgunjal45@gmail.com>
2025-06-16 21:06:37 +0530

    Add day 23

---
+++ 03_JS/Day23/string.js
@@ -0,0 +1,13 @@
+// strings oprations
+// let s1 = "Hello everyone"
+// console.log(s1.slice(1,3));
+// console.log(s1.substring(0,3));
+
+// console.log(s1.replace("Hello", "Hi")); //replace-->used to replace first word that aapears in string, if you want to replace all appred value
+// then use .replaceAll
+
+// let s2 = "one, two, three, four"
+// console.log(s2.split(", "));
+
+let s3 = "      one, two, three, four"
+console.log(s3.trim()); // trin space from start and end
