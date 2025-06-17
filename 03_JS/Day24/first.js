cf0436bf5308a8ce91d52f76217270d8f349d24f
gaurav-gunjal14 <gauravgunjal45@gmail.com>
2025-06-17 11:59:37 +0530

    Add day 24

---
+++ 03_JS/Day24/first.js
@@ -0,0 +1,25 @@
+// Numbers
+// let n1 = 321.66;
+// console.log(n1.toFixed(1));
+// console.log(n1.toPrecision(1));
+// console.log(n1.toExponential(1));
+// console.log(n1.toString());
+
+
+//Math
+// console.log(Math.E)
+// console.log(Math.LN10)
+// console.log(Math.PI)
+
+// console.log(Math.floor(n1));
+// console.log(Math.ceil(n1));
+//0-9
+// console.log(Math.floor(Math.random()*10)); //  math.random--> 0<x<1
+//1-10
+// console.log(Math.floor(Math.random()*10)+1);
+//11-20
+// console.log(Math.floor(Math.random()*10)+11);
+//formula console.log(Math.floor(Math.random()*(max-min+1)+min);
+
+// 1-6
+console.log(Math.floor(Math.random()*6)+1);
---
+++ 03_JS/Day24/second.js
@@ -0,0 +1,44 @@
+// array
+// let arr1 = [1,2,3,4,"gaurav",true,3];
+
+// console.log(arr1[1]);
+// console.log(arr1.at(2)); // this is letest and it can also take -ve index
+// console.log(arr1.length);
+
+// const arr2 = structuredClone(arr1);
+// console.log(arr2);
+
+// arr1.push(30);
+// console.log(arr1)
+// arr1.pop(30);
+// console.log(arr1);
+// arr1.unshift(30);
+// console.log(arr1);
+// arr1.shift();
+// console.log(arr1);
+
+// delete arr1[0];
+// console.log(arr1);
+
+// console.log(arr1.indexOf(3));
+// console.log(arr1.lastIndexOf(3));
+// console.log(arr1.includes(1));
+
+// console.log(arr1.slice(2, 5));
+// console.log(arr1.splice(2, 5));
+// console.log(arr1);
+// console.log(arr1.toString());
+// console.log(arr1.join("*"));
+
+// let arr2 = (6);
+// let arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+
+// 2D arry
+let arr2d = [[1,2,3], [4,5,6], [7,8, 9]];
+// console.log(arr2d[0][2]);
+
+// let newarr = arr2d.flat(1);
+// console.log(newarr);
+console.log(Array.isArray(arr2d));
