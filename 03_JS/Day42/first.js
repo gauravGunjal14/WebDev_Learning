3d0a1f112c1d8a8dc27e5f1e24584ab2952f81bf
gaurav-gunjal14 <gauravgunjal45@gmail.com>
2025-07-09 21:23:24 +0530

    Add new code in Day 42

--- 03_JS/Day42/first.js
+++ 03_JS/Day42/first.js
@@ -27,3 +27,34 @@ const form = document.querySelector("form");
 //     console.log("Form reset");
 // })
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent the default form submission
+
+    // const first = document.getElementById("first");
+    // console.log(first.value);
+
+    // const second = document.getElementById("last");
+    // console.log(second.value);
+
+    // const age = document.getElementById("age");
+    // console.log(age.value);
+
+    const data = new FormData(form);
+    // console.log(data);
+
+    // console.log(Array.from(data.keys()));
+    // console.log(Array.from(data.values()));
+
+    // for(let key of data.keys()) {
+    //     console.log(key);
+    // }
+
+    for(let key of data.entries()){
+        console.log(key);
+    }
+
+    // const result = document.getElementById("result");
+    // result.innerText = `${first.value} ${second.value} is ${age.value} years old.`;
+
+    // document.body.append(result);
+});
