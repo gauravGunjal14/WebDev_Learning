19931a458cac182ddf4b4a770f1c0b2ea2faa5b1
gaurav-gunjal14 <gauravgunjal45@gmail.com>
2025-07-18 20:35:37 +0530

    Add day 43

--- 03_JS/Day43/first.js
+++ 03_JS/Day43/first.js
@@ -1,4 +1,13 @@
-const original_answers = ["Sachin Tendulkar", "West Indies", "10", "Sachin Tendulkar", "11"];
+// const original_answers = ["Sachin Tendulkar", "West Indies", "10", "Sachin Tendulkar", "11"];
+
+const original_answers = {
+    q1: "Sachin Tendulkar",
+    q2: "West Indies",
+    q3: "10",
+    q4: "Sachin Tendulkar",
+    q5: "11"
+};
+
 
 
 const form = document.querySelector("form");
@@ -7,18 +16,24 @@ form.addEventListener("submit", (event) => {
     event.preventDefault(); // Prevent the default form submission behavior
     const data = new FormData(form);
 
-    const answer = Array.from(data.values());
+    // const answer = Array.from(data.values());
     
+    // let result = 0;
+
+    // for(let i=0; i< original_answers.length; i++){
+    //     if(answer[i]=== original_answers[i]){
+    //         result++;
+    //     }
+    // }
+
     let result = 0;
 
-    for(let i=0; i< original_answers.length; i++){
-        if(answer[i]=== original_answers[i]){
+    for(let [key, values] of data.entries()){
+        if(values === original_answers[key]){
             result++;
         }
     }
-
     const out = document.getElementById("out");
     out.innerText = `${result} out of 5`;
 
-    document.getElementById("container").append(out);
 })
