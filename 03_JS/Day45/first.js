64d300119d814971b78fad76a9650676e56430b1
gaurav-gunjal14 <gauravgunjal45@gmail.com>
2025-07-28 22:07:59 +0530

    created tic tac toe

--- 03_JS/Day45/first.js
+++ 03_JS/Day45/first.js
@@ -1,29 +1,77 @@
 let turn = "O";
-
-// Patterns
-let winner = [
-    [1,2,3],[4,5,6],[7,8,9],
-    [1,4,7],[2,5,8],[3,6,9],
-    [1,5,9],[3,5,7]
-]
+total_turn = 0;
 
 const board_value = new Array(9).fill("E");
 
 const board = document.querySelector(".board");
 
+const winnerMsg = document.querySelector("#winnerMsg");
+
 // printing O and X
-board.addEventListener("click", (event)=>{
-    // console.log(event.target);
-    
+const printer = (event) => {
     const element = event.target;
-    if(turn === "O"){
-        element.innerHTML = "O";
-        board_value[element.id] = "O";
-        turn = "X";
+    total_turn++;
+    if (board_value[element.id] === "E") {
+
+        if (turn === "O") {
+            element.innerHTML = "O";
+            board_value[element.id] = "O";
+            if (checkWinner()) {
+                winnerMsg.innerHTML = "Winner is O";
+                board.removeEventListener("click", printer);
+                return;
+            }
+            turn = "X";
+        }
+        else {
+            element.innerHTML = "X";
+            board_value[element.id] = "X";
+            if (checkWinner()) {
+                winnerMsg.innerHTML = "Winner is X";
+                board.removeEventListener("click", printer);
+                return;
+            }
+            turn = "O";
+        }
+        if (total_turn == 9) {
+            winnerMsg.innerHTML = "Match is Draw";
+        }
     }
-    else{
-        element.innerHTML = "X";
-        board_value[element.id] = "X";
-        turn = "O";
+}
+
+board.addEventListener("click", printer);
+
+// Patterns
+let winner = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8],
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6]
+];
+
+// checking for winner
+function checkWinner() {
+    for (let [index0, index1, index2] of winner) {
+        // console.log(index0, index1, index2)
+        if (
+            board_value[index0] !== "E" &&
+            board_value[index0] === board_value[index1] &&
+            board_value[index1] === board_value[index2]
+        ) {
+            return 1;
+        }
     }
+}
+
+// restar button
+const restart = document.getElementById("restart");
+restart.addEventListener("click", () => {
+    const cell = document.getElementsByClassName("cell");
+    Array.from(cell).forEach((value) => {
+        value.innerHTML = "";
+    });
+    turn = "O";
+    total_turn = 0;
+    board_value = new Array(9).fill("E");
+    winnerMsg.innerHTML = "";
+    board.addEventListener("click", printer);
 });
