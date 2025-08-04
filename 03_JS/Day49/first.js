a148194cf6f075561402b9bfc86f33335add6e0d
Gaurav Gunjal <gauravgunjal45@gmail.com>
2025-08-04 20:59:17 +0530

    studied about Async Task working and Web API

---
+++ 03_JS/Day49/first.js
@@ -0,0 +1,16 @@
+console.log("Hello");
+
+function meet() {
+    const arr = [1, 2, 3];
+    console.log(arr[1]);
+}
+
+function greet(name) {
+    const a = 2+3;
+    console.log(a);
+    meet();
+    console.log(a*a);
+}
+
+greet();
+console.log("End of script");
