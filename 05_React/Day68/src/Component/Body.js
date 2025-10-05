57b0f2b0907aeedf67ba19fa00a155de45100e2c
Gaurav Gunjal <gauravgunjal45@gmail.com>
2025-10-05 20:40:58 +0530

    completed github profile mini-project

--- 05_React/Day68/src/Component/Body.js
+++ 05_React/Day68/src/Component/Body.js
@@ -1,35 +1,44 @@
 import { useEffect, useState } from "react"
 
-function Body(){
+function Body() {
 
-        const [Profile,setProfile] = useState([]);
+    const [Profile, setProfile] = useState([]);
+    const [Novari, setNoVari] = useState("");
 
-        async function generateProfile(){
-            const response = await fetch("https://api.github.com/users?per_page=10");
-            const data = await response.json();
+    async function generateProfile(count) {
+        const random = Math.floor(Math.random()*10000);
+        
+        const response = await fetch(`https://api.github.com/users?since=${random})}&per_page=${count}`);
+        const data = await response.json();
 
-            setProfile(data);
-        }
+        setProfile(data);
+    }
 
-        useEffect(()=>{
-            generateProfile();
-        },[]);
+    useEffect(() => {
+        generateProfile(10);
+    }, []);
 
-        return(
+    return (
+        <div>
+            <div className="search-Profile">
+                <input type="text" id="no" placeholder="Search here" value={Novari} min={1} max={50} onChange={(e)=>{setNoVari(e.target.value)}}></input>
+                <button onClick={()=>generateProfile(Number(Novari))}>Search Profile</button>
+            </div>
             <div className="profiles">
                 {
-                    Profile.map((value)=>{
-                        return(
-                        <div key={value.id} className="cards">
-                            <img src={value.avatar_url}></img>
-                            <h2>{value.login}</h2>
-                            <a href={value.html_url} target="_blank">Profile</a>
-                        </div>
+                    Profile.map((value) => {
+                        return (
+                            <div key={value.id} className="cards">
+                                <img src={value.avatar_url}></img>
+                                <h2>{value.login}</h2>
+                                <a href={value.html_url} target="_blank">Profile</a>
+                            </div>
                         )
                     })
                 }
             </div>
-        )
+        </div>
+    )
 }
 
 export default Body;
