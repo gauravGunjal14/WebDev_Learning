fb513537f9db368c06fcc02488065c7422fdf633
Gaurav Gunjal <gauravgunjal45@gmail.com>
2025-12-13 21:39:48 +0530

    day 81

--- 07_GSAP/Day81/script.js
+++ 07_GSAP/Day81/script.js
@@ -1,12 +1,30 @@
-gsap.to("#box1", {
+// gsap.to("#box1", {
+//     x: 1000,
+//     duration: 2,
+//     delay: 1,
+//     redius: 50,
+// })
+
+// gsap.from("#box2", {
+//     x: 1000,
+//     duration: 2,
+//     delay: 1,
+//     rotate: 360,
+//     borderRadius: "50%",
+// })
+
+let tl = gsap.timeline();
+
+tl.to("#box1", {
     x: 1000,
     duration: 2,
     delay: 1,
+    borderRadius: "50%",
 })
 
-gsap.from("#box2", {
+tl.from("#box2", {
     x: 1000,
     duration: 2,
-    delay: 1,
     rotate: 360,
+    borderRadius: "50%",
 })
