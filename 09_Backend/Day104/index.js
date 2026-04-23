f00260a4bae1024c45905dc7b33fd94382a44f37
Gaurav Gunjal <gauravgunjal45@gmail.com>
2026-04-23 20:07:30 +0530

    chat app and room chat

--- 09_Backend/Day104/index.js
+++ 09_Backend/Day104/index.js
@@ -1,18 +1,34 @@
 const { Server } = require('socket.io');
-const app = require('express');
+const express = require('express');
+const app = express();
 const http = require('http');
+const path = require('path');
 
 const server = http.createServer(app);
 const io = new Server(server);
+server.listen(3000, () => {
+    console.log('listening on 3000');
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 io.on('connection', (socket) => {
     console.log('a user connected');
     socket.on('disconnect', () => {
         console.log('user disconnected');
-    }
-    );
-    socket.on('message', (msg) => {
-        io.emit('new-message', msg);
-    }
-    );
+    });
+    
+    // socket.on('message', (msg) => {
+    //     socket.broadcast.emit('new-message', msg);
+    // });
+
+    socket.on('message', ({room, msg}) => {
+        socket.to(room).emit('new-message', msg);
+    });
+
+    socket.on('join-room', (room) => {
+        socket.join(room);
+    });
 });
